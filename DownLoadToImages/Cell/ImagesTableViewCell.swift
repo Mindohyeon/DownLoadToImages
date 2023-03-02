9d51281f4bb129e1bd3f91432c3a673364b148ee
Mindohyeon <min50875@naver.com>
2023-03-02 12:21:59 +0900

    commit

---
+++ DownLoadToImages/Cell/ImagesTableViewCell.swift
@@ -0,0 +1,82 @@
+import UIKit
+import SnapKit
+import Then
+class ImagesTableViewCell: UITableViewCell {
+    static let identifier = "ImageTableViewCell"
+    
+    weak var delegate: ImageDelegate?
+
+    private let downLoadImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "photo")
+        $0.sizeToFit()
+    }
+    
+    private let progressBarLine = UIProgressView().then {
+        $0.progress = 0.5
+        $0.progressTintColor = .blue
+        $0.trackTintColor = .lightGray
+    }
+    
+    lazy var loadButton = UIButton().then {
+        $0.addTarget(self, action: #selector(loadButtonDidTap(_:)), for: .touchUpInside)
+        $0.layer.cornerRadius = 8
+        $0.backgroundColor = .blue
+        $0.setTitleColor(.white, for: .normal)
+        $0.setTitle("Load", for: .normal)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addView()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func loadButtonDidTap(_ sender: UIButton) {
+        var url = ""
+        switch sender.tag {
+        case 0:
+            url = "https://digitalauto.hyundaicapital.com//assets/images/potal/mainCarImg/IGJS.png"
+        case 1:
+            url = "http://www.dailyenews.co.kr/news/photo/202105/22354_19656_1216.jpg"
+        case 2:
+            url = "https://www.hyundai.com/contents/repn-car/side-45/avante-22my-45side.png"
+        case 3:
+            url = "https://blog.kakaocdn.net/dn/wH7Q6/btrh2ipZouW/siYgr5GUAHpr4NV3ib4Gyk/img.jpg"
+        default:
+            print("default")
+        }
+        
+        downLoadImageView.load(url: url)
+    }
+    
+    private func addView() {
+        contentView.addSubviews(downLoadImageView, progressBarLine, loadButton)
+    }
+    
+    private func setLayout() {
+        downLoadImageView.snp.makeConstraints {
+            $0.width.equalTo(90)
+            $0.top.equalToSuperview().inset(3)
+            $0.leading.equalToSuperview().inset(30)
+            $0.bottom.equalToSuperview().inset(3)
+        }
+        
+        progressBarLine.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(downLoadImageView.snp.trailing).offset(5)
+            $0.trailing.equalTo(loadButton.snp.leading)
+        }
+        
+        loadButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(30)
+            $0.width.equalTo(70)
+            $0.height.equalTo(35)
+        }
+    }
+}
---
+++ DownLoadToImages/ViewController.swift
@@ -0,0 +1,61 @@
+import UIKit
+import SnapKit
+import Then
+
+class ViewController: UIViewController, ImageDelegate {
+    func loadButtonDidTap(cell: ImagesTableViewCell) {
+        
+    }
+    
+    
+    private let imageTableView = UITableView().then {
+        $0.rowHeight = 70
+        $0.register(ImagesTableViewCell.self, forCellReuseIdentifier: ImagesTableViewCell.identifier)
+    }
+    
+    private let loadAllButton = UIButton().then {
+        $0.layer.cornerRadius = 7
+        $0.backgroundColor = .blue
+        $0.setTitleColor(.white, for: .normal)
+        $0.setTitle("Load All Images", for: .normal)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        imageTableView.dataSource = self
+        
+        addView()
+        setLayout()
+    }
+
+    func addView() {
+        view.addSubviews(imageTableView, loadAllButton)
+    }
+    
+    func setLayout() {
+        imageTableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(2)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(loadAllButton.snp.top).inset(10)
+        }
+        
+        loadAllButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(350)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(50)
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ImagesTableViewCell.identifier, for: indexPath) as? ImagesTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+
+}
